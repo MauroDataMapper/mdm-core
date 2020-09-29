60e699f95b1cdd2a20d744fbe2533e668918a61c
Christina <ings.christina@gmail.com>
2020-09-29 17:28:59 +0100

    Create class for Model "Targets"

---
+++ mdm-core/grails-app/utils/uk/ac/ox/softeng/maurodatamapper/core/rest/transport/model/ModelLinkTarget.groovy
@@ -0,0 +1,13 @@
+package uk.ac.ox.softeng.maurodatamapper.core.rest.transport.model
+
+import uk.ac.ox.softeng.maurodatamapper.core.facet.VersionLinkType
+
+class ModelLinkTarget {
+    String modelId
+    String description
+
+    ModelLinkTarget(UUID targetId, VersionLinkType versionLinkType) {
+        modelId = targetId.toString()
+        this.description = versionLinkType.toString()
+    }
+}
