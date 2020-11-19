4845c5f37674ea75e7ce85513aae0ea31edd47ba
Ollie Freeman <ollie.freeman@gmail.com>
2020-11-19 17:04:28 +0000

    Add the api key authentication plugin
    This will allow api key authentication on all requests
    Add trait to include common methods for managing the security policy manager in interceptors
    Also dont set the current policy if its already set, this will allow many
    different management systems to work without having to override another
    it will just matter the order in which the interceptor is used

---
+++ mdm-plugin-authentication-apikey/grails-app/controllers/uk/ac/ox/softeng/maurodatamapper/authentication/apikey/ApiKeyAuthenticationInterceptor.groovy
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package uk.ac.ox.softeng.maurodatamapper.authentication.apikey
+
+import uk.ac.ox.softeng.maurodatamapper.core.session.SessionService
+import uk.ac.ox.softeng.maurodatamapper.security.CatalogueUser
+import uk.ac.ox.softeng.maurodatamapper.security.UserSecurityPolicyManager
+import uk.ac.ox.softeng.maurodatamapper.security.UserSecurityPolicyManagerInterceptor
+import uk.ac.ox.softeng.maurodatamapper.security.interceptor.SecurityPolicyManagerInterceptor
+
+class ApiKeyAuthenticationInterceptor implements SecurityPolicyManagerInterceptor {
+
+    public static final String API_KEY_HEADER = 'apiKey'
+
+    ApiKeyAuthenticatingService apiKeyAuthenticatingService
+    SessionService sessionService
+
+    ApiKeyAuthenticationInterceptor() {
+        match(uri: '/**/api/**/')
+            .excludes(controller: 'authenticating', action: 'login')
+            .excludes(controller: 'authenticating', action: 'logout')
+        // We want to authenticate before we try to load the usersecuritypolicy manager
+        order = UserSecurityPolicyManagerInterceptor.ORDER - 100
+    }
+
+    boolean before() {
+
+        checkSessionIsValid()
+
+        if (!securityPolicyManagerIsSet()) {
+            String apikeyHeader = request.getHeader(API_KEY_HEADER)
+            // No header then just carry on as if a normal request
+            if (!apikeyHeader) return true
+
+            CatalogueUser authenticatedUser = apiKeyAuthenticatingService.authenticateAndObtainUserUsingApiKey(apikeyHeader)
+
+            // Api Key hasn't authenticated against anything so just proceed as if a normal request
+        if (!authenticatedUser) return true
+
+            UserSecurityPolicyManager userSecurityPolicyManager =
+                apiKeyAuthenticatingService.retrieveOrBuildUserSecurityPolicyManager(authenticatedUser, session)
+
+            setCurrentUserSecurityPolicyManager(userSecurityPolicyManager)
+        }
+        true
+    }
+}
---
+++ mdm-security/src/main/groovy/uk/ac/ox/softeng/maurodatamapper/security/interceptor/SecurityPolicyManagerInterceptor.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package uk.ac.ox.softeng.maurodatamapper.security.interceptor
+
+import uk.ac.ox.softeng.maurodatamapper.core.traits.controller.MdmInterceptor
+
+import javax.servlet.http.HttpSession
+
+/**
+ * @since 13/01/2021
+ */
+trait SecurityPolicyManagerInterceptor extends MdmInterceptor {
+
+    void checkSessionIsValid() {
+        // Get the session without using the grails wrapper
+        // If we use 'session' it sets the session variable and we cannot change it
+        HttpSession localSession = getRequest().getSession()
+        // Check if its invalid (this is only likely to happen when running in dev mode and we shut down the backend and then
+        // make another request on startup with the old session in the request
+        // Can be tested easily enough with postman)
+        // Validity is tested by existence in the known session id map, which is updated whenever tomcat creates a new session
+        boolean invalid = sessionService.isInvalidatedSession(localSession)
+
+        // If invalid then invalidate the local session and force the request to create a new one
+        // Upon creation of new session the session will be added to the map and all subsequent checks will come back as a valid but new session
+        if (invalid) {
+            localSession.invalidate()
+            getRequest().getSession(true)
+        }
+    }
+
+    boolean securityPolicyManagerIsSet() {
+        params.currentUserSecurityPolicyManager
+    }
+}
