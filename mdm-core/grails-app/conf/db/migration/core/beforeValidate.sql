f715e4c32357bfe3f6a480e540498461767245a2
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-06-30 11:48:28 +0100

    Fix the flyway metadata migration script
    On a new install the tables expected dont exist in the core mirgation, therefore this script needs to be in datamodel plugin
    Also add the extension in the beforeMigrate callback so that its always available no matter what

--- mdm-core/grails-app/conf/db/migration/core/beforeValidate.sql
+++ mdm-core/grails-app/conf/db/migration/core/beforeValidate.sql
@@ -1,4 +1,9 @@
 UPDATE core.flyway_schema_history
 SET checksum = 490568862
 WHERE version = '1.7.0' AND
-      checksum = -1413608684
+      checksum = -1413608684;
+
+DELETE
+FROM core.flyway_schema_history
+WHERE version = '2.10.0' AND
+      description = 'update database metadata values';
