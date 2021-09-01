fd76eb26d924d5cc8b06b4d8c22f365eb7c60570
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2021-09-01 17:06:36 +0100

    Make sure the authority export does not include whether the authority is default or not

--- mdm-plugin-referencedata/grails-app/domain/uk/ac/ox/softeng/maurodatamapper/referencedata/ReferenceDataModel.groovy
+++ mdm-plugin-referencedata/grails-app/domain/uk/ac/ox/softeng/maurodatamapper/referencedata/ReferenceDataModel.groovy
@@ -82,6 +82,7 @@ class ReferenceDataModel implements Model<ReferenceDataModel>, ReferenceSummaryM
         documentationVersion type: VersionUserType
         modelVersion type: VersionUserType
         folder cascade: 'none'
+        authority fetch: 'join'
         referenceDataTypes cascade: 'all-delete-orphan', cascadeValidate: 'dirty'
         referenceDataElements cascade: 'all-delete-orphan', cascadeValidate: 'dirty'
         referenceDataValues cascade: 'all-delete-orphan', cascadeValidate: 'dirty'
