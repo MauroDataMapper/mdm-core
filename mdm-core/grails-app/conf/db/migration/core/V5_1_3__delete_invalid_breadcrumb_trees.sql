aee6a9ef3cb76fb0478a46b7b5f41dbcf3195d36
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2022-08-04 18:45:36 +0100

    Update BreadcrumbTree cleanup migration with faster version

--- mdm-core/grails-app/conf/db/migration/core/V5_1_3__delete_invalid_breadcrumb_trees.sql
+++ mdm-core/grails-app/conf/db/migration/core/V5_1_3__delete_invalid_breadcrumb_trees.sql
@@ -1,22 +1,24 @@
 -- before mdm-core version 5.2.0, child BreadcrumbTrees were not deleted when parent models were deleted
 -- clean up orphaned BreadcrumbTrees
 
+CREATE TEMPORARY TABLE valid_breadcrumb_trees_temp AS
 WITH RECURSIVE valid_breadcrumb_trees AS MATERIALIZED (
-    SELECT id,
-           parent_id
+    SELECT *
     FROM core.breadcrumb_tree
     WHERE top_breadcrumb_tree = TRUE
     UNION ALL
-    SELECT c.id,
-           c.parent_id
+    SELECT c.*
     FROM core.breadcrumb_tree c
          INNER JOIN valid_breadcrumb_trees v
                     ON c.parent_id = v.id
 )
-DELETE
-FROM core.breadcrumb_tree c
-WHERE NOT EXISTS(
-        SELECT NULL
-        FROM valid_breadcrumb_trees v
-        WHERE c.id = v.id
-    );
+SELECT *
+FROM valid_breadcrumb_trees;
+
+ALTER TABLE core.breadcrumb_tree DISABLE TRIGGER ALL;
+DELETE FROM core.breadcrumb_tree;
+ALTER TABLE core.breadcrumb_tree ENABLE TRIGGER ALL;
+
+INSERT INTO core.breadcrumb_tree
+SELECT *
+FROM valid_breadcrumb_trees_temp;
