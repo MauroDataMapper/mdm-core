8f0da5e36da41d09f8d57cb8ff0b55965348d7af
Oleksii <oleksi.rybak@gmail.com>
2023-02-09 11:08:23 +0000

    fixing issue + checkstyle

--- mdm-core/grails-app/conf/db/migration/core/V5_1_3__delete_invalid_breadcrumb_trees.sql
+++ mdm-core/grails-app/conf/db/migration/core/V5_1_3__delete_invalid_breadcrumb_trees.sql
@@ -1,16 +1,16 @@
 -- before mdm-core version 5.2.0, child BreadcrumbTrees were not deleted when parent models were deleted
 -- clean up orphaned BreadcrumbTrees
 
-CREATE TEMPORARY TABLE valid_breadcrumb_trees_temp AS
+CREATE TEMPORARY TABLE IF NOT EXISTS valid_breadcrumb_trees_temp AS
 WITH RECURSIVE valid_breadcrumb_trees AS MATERIALIZED (
-		SELECT *
-		FROM core.breadcrumb_tree
-		WHERE top_breadcrumb_tree = TRUE
-		UNION ALL
-		SELECT c.*
-		FROM core.breadcrumb_tree c
-			 INNER JOIN valid_breadcrumb_trees v
-						ON c.parent_id = v.id
+    SELECT *
+    FROM core.breadcrumb_tree
+    WHERE top_breadcrumb_tree = TRUE
+    UNION ALL
+    SELECT c.*
+    FROM core.breadcrumb_tree c
+      INNER JOIN valid_breadcrumb_trees v
+                    ON c.parent_id = v.id
 )
 
 SELECT * FROM valid_breadcrumb_trees;
@@ -39,4 +39,4 @@ $$;
 INSERT INTO core.breadcrumb_tree
 SELECT * FROM valid_breadcrumb_trees_temp;
 
-CREATE INDEX breadcrumb_tree_tree_string_idx ON core.breadcrumb_tree (tree_string text_pattern_ops);
+CREATE INDEX IF NOT EXISTS breadcrumb_tree_tree_string_idx ON core.breadcrumb_tree (tree_string text_pattern_ops);
