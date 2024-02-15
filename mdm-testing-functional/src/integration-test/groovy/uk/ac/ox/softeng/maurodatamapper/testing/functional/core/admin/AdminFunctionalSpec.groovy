068f36cfeab20ee82c677b1794a81fe6f209c64f
James Welch <james.welch@cs.ox.ac.uk>
2024-02-15 14:49:51 +0000

    Fix failing Java version test

--- mdm-testing-functional/src/integration-test/groovy/uk/ac/ox/softeng/maurodatamapper/testing/functional/core/admin/AdminFunctionalSpec.groovy
+++ mdm-testing-functional/src/integration-test/groovy/uk/ac/ox/softeng/maurodatamapper/testing/functional/core/admin/AdminFunctionalSpec.groovy
@@ -80,7 +80,7 @@ class AdminFunctionalSpec extends FunctionalSpec {
 {
   "Mauro Data Mapper Version": "${json-unit.matches:version}",
   "Grails Version": "5.3.2",
-  "Java Version": "17.0.6",
+  "Java Version": "${json-unit.regex}17.*",
   "Java Vendor": "${json-unit.any-string}",
   "OS Name": "${json-unit.any-string}",
   "OS Version": "${json-unit.matches:version}",
