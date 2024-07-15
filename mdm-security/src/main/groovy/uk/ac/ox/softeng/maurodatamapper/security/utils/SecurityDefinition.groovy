ee62fc8b4a70b4b0014691e6959d5a9a794b5e7f
James Welch <james.welch@bdi.ox.ac.uk>
2024-07-15 14:33:25 +0100

    Slightly more intelligently looking for an administrators group

--- mdm-security/src/main/groovy/uk/ac/ox/softeng/maurodatamapper/security/utils/SecurityDefinition.groovy
+++ mdm-security/src/main/groovy/uk/ac/ox/softeng/maurodatamapper/security/utils/SecurityDefinition.groovy
@@ -165,7 +165,7 @@ trait SecurityDefinition {
     }
 
     void getOrCreateBasicGroups(String creatorKey, boolean includeAdmin = true) {
-        admins = UserGroup.findByNameInList(['administrators', 'Administrators'])
+        admins = getAdministratorsGroup()
         if (includeAdmin) {
             if (!admins) createAdminGroup(creatorKey)
         }
@@ -206,4 +206,10 @@ trait SecurityDefinition {
         }
         admins.addToGroupMembers(creator)
     }
+
+    UserGroup getAdministratorsGroup() {
+        GroupRole adminGroupRole = GroupRole.findByName(GroupRole.SITE_ADMIN_ROLE_NAME)
+        UserGroup.findByApplicationGroupRole(adminGroupRole)
+    }
+
 }
